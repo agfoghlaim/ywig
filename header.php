79f7b60cedc901dc78fcfc6f0329be01790c8d83
agfoghlaim <marieohara.gti@gmail.com>
2020-03-26 12:31:50 +0000

    Mechanics of Youth Clubs Section works. YouthClubs CPT.

--- header.php
+++ header.php
@@ -204,3 +204,23 @@ $socials = array($twitter, $facebook, $youtube);
     <?php require_once( get_template_directory() . '/ywig-frontpage-sections/locations.php'); ?>
 
   </section>
+
+  <section class="youth-clubs" id="youth-clubs">
+    <div class="section-title-wrap">
+      <div class="row ywig-section-title-row">
+        <img src="http://youthworkgalway.ie/wp-content/uploads/2018/06/ywig_tiny_colour.png">
+      </div>
+      <div class="row ywig-section-title-row">
+        <h2 class="dark-text">Youth Clubs</h2>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="offset-lg-3 col-lg-6 offset-md-3 col-md-6">
+        <p class="text-center">This needs to be editable in the theme customizer under 'Youth Clubs Section'.</p>
+      </div>
+    </div>
+    
+    <?php require_once( get_template_directory() . '/ywig-frontpage-sections/youth-clubs.php'); ?>
+
+  </section>
