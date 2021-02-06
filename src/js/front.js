7d4effde42847aa74d37bc211c417f6ee800dcb7
agfoghlaim <marieohara.gti@gmail.com>
2021-02-06 10:15:11 +0000

    Style buttons, move header change js to app.js.

--- src/js/front.js
+++ src/js/front.js
@@ -137,24 +137,3 @@ const scrollTo = () => {
 scrollTo();
 // export default scrollTo;
 
-/*
-
-Header -add bg color on scroll
-
-*/
-
-document.addEventListener('scroll', handleHeaderBgColor);
-
-const header = document.getElementById('home');
-
-const headerHeight = document.getElementsByTagName('header')[0].offsetHeight;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function handleHeaderBgColor() {
-  if (!header) return;
-  if (window.pageYOffset > headerHeight) {
-    header.classList.add('with-bg-color');
-  } else {
-    header.classList.remove('with-bg-color');
-  }
-}
